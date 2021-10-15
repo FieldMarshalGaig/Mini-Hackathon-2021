c3cf084821de67d233533233b607f91c74a4fe74
FieldMarshalGaig <59373022+FieldMarshalGaig@users.noreply.github.com>
2021-10-15 19:59:09 +0100

    Added html and js files

---
+++ memoryGame.js
@@ -0,0 +1,17 @@
+/*   Event listeners   */
+//Changes canvas size whenever the screen size is changed
+window.addEventListener("resize",fctnChangeSize);
+
+//Listens for key presses and takes appropriate action
+window.addEventListener("keydown",function(event){
+    if (event.key == "w"){
+
+    } else if (event.key == "a") {
+        
+    } else if (event.key == "d") {
+        
+    }
+
+
+
+},false)
